19428d6af9438b1244e9fdc404d30a38c13310b7
Shane Mc Cormack <shanemcc@gmail.com>
2017-12-30 06:57:41 +0000

    Fix RSS Urls.

--- static/js/override.js
+++ static/js/override.js
@@ -12,3 +12,16 @@ gtag('config', 'UA-2963238-1');
 	var x = document.getElementsByTagName('script')[0];
 	x.parentNode.insertBefore(s, x);
 })();
+
+/** A.DF.VG */
+var _paq = _paq || [];
+/* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+_paq.push(['trackPageView']);
+_paq.push(['enableLinkTracking']);
+(function() {
+	var u="//a.df.vg/";
+	_paq.push(['setTrackerUrl', u+'piwik.php']);
+	_paq.push(['setSiteId', '1']);
+	var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+	g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+})();
