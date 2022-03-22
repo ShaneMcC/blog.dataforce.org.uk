7124953be52302dae57fce2e0de4208168d7ba26
Shane Mc Cormack <shanemcc@gmail.com>
2022-03-22 06:15:55 +0000

    Make purgecss actually check posts as well so that it doesn't purge code styles...

--- tidy.sh
+++ tidy.sh
@@ -21,15 +21,14 @@ fi;
 if [ -e "${CWEBP}" ]; then
 	for FILE in $(find . -name '*.jpg' -o -name '*.png' -o -name '*.jpeg'); do
 		if [ ! -e "${FILE}.webp" ]; then
-			${CWEBP} -m 6 -mt -o "${FILE}.webp" -- "${FILE}"
+			${CWEBP} -m 6 -mt -o "${FILE}.webp" -- "${FILE}" || true
 		fi;
 	done
 fi;
 
 # Remove unused CSS
 if [ -e "${PURGECSS}" ]; then
-	${PURGECSS} --output css/ --css css/style.min.*.css --content '**.html'
-	ls css/
+	${PURGECSS} --variables --font-face --keyframes --output css/ --css css/style.min.*.css --content '**.html' '**/*.html'
 fi;
 
 rm __postcss-dummy*.html
