f434ae19a120788e02c48b808ea58de8bb77e443
Shane Mc Cormack <shanemcc@gmail.com>
2025-06-19 01:50:56 +0100

    Add some sleeping

--- content/posts/2024/11/23-convert-lxc-container-to-vm/outer_script.sh
+++ content/posts/2024/11/23-convert-lxc-container-to-vm/outer_script.sh
@@ -8,7 +8,9 @@ for F in /dev /sys /dev/pts /proc; do mount -o bind {,/mnt}${F}; done
 chroot /mnt sh -c "wget -O /tmp/convert.sh https://blog.dataforce.org.uk/2024/11/convert-lxc-container-to-vm/convert_script.sh && chmod a+x /tmp/convert.sh && /tmp/convert.sh"
 
 if [ $? -eq 0 ]; then
-    for F in /dev/pts; do umount /mnt${F}; sleep 5; done
+    sleep 5;
+    for F in /dev/pts; do umount /mnt${F}; done
+    sleep 5
     for F in /dev /sys /proc; do umount /mnt${F}; done
     umount /mnt
     reboot
